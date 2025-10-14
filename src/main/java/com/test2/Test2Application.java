41a2924c79be5d26e8716554083d049ebc099901
kishanreddy2003 <kishanreddy226@gmail.com>
2025-10-14 13:25:41 +0530

    Update Test2Application.java

--- src/main/java/com/test2/Test2Application.java
+++ src/main/java/com/test2/Test2Application.java
@@ -9,6 +9,7 @@ public class Test2Application {
 	public static void main(String[] args) {
 		int x=100;
 		String name="kishan";
+		System.out.println("Hello world");
 		int x=200;
 		String name="prabaht";
 		SpringApplication.run(Test2Application.class, args);
