812247055214e86c2d9a59044ccdb71752dd6f33
kishan <kishanreddy226@gmail.com>
2025-10-14 13:17:52 +0530

    added

--- src/main/java/com/test2/Test2Application.java
+++ src/main/java/com/test2/Test2Application.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test2Application {
 
 	public static void main(String[] args) {
+		int x=200;
+		String name="prabaht";
 		SpringApplication.run(Test2Application.class, args);
 	}
 
